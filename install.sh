d13eecaac15f5be70c2de83e7a3cc3cc21874074
Firebase vps <firebasevps@gmail.com>
2025-04-13 09:44:49 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 yes | sudo apt install python3.10
 # Qbittorrent
-sudo apt install dirmngr ca-certificates software-properties-common apt-transport-https
+yes | sudo apt install dirmngr ca-certificates software-properties-common apt-transport-https
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 sudo apt update
 yes | sudo apt install qbittorrent
