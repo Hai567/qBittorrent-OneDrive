6fab857f025350fcccef402d9003262322cbf253
Firebase vps <firebasevps@gmail.com>
2025-04-13 05:16:04 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 sudo apt update && sudo apt upgrade
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
-sudo apt install python3.10
+yes | sudo apt install python3.10
 # Qbittorrent
 sudo apt install dirmngr ca-certificates software-properties-common apt-transport-https
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 sudo apt update
-sudo apt install qbittorrent
+yes | sudo apt install qbittorrent
+# Rclone
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
